1fabd2ddea9801386807dffff340275509c471fd
Kristin Cheng Wu <kristin.cheng.wu@gmail.com>
2025-02-20 22:33:39 -0500

    unit testing connected PCBs

--- smd_test/unit_test/unit_test.ino
+++ smd_test/unit_test/unit_test.ino
@@ -5,57 +5,67 @@
 #include <Streaming.h>
 #include <Adafruit_TinyUSB.h>
 
+const int X_LEN = 48;
+const int Y_LEN = 24;
+const int DELAY = 1000; // 1 second
+const int NUM_SELECT = 6;
+
 const int ENABLE_X_PIN = 5;             
 const int ENABLE_Y_PIN = 2;             
-const int SELECT_PINS[6] = {13, 12, 11, 10, 9, 7}; // S5/S2-2~13, S0/S0-1~7
+const int SELECT_PINS[NUM_SELECT] = {13, 12, 11, 10, 9, 7}; // S5/S2-2~13, S0/S0-1~7
 const int OUTPUT_PIN = A0;
 
-int testing_x = false; 
 int touch_value = 0;  
 
 void setup() {
   Serial.begin(115200);
   pinMode(OUTPUT_PIN, INPUT);
 
-  for (int i=0; i<6; i++) {
+  for (int i = 0; i < NUM_SELECT; i++) {
     pinMode(SELECT_PINS[i], OUTPUT);
     digitalWrite(SELECT_PINS[i], LOW);
   }
 
   pinMode(ENABLE_X_PIN, OUTPUT);
   pinMode(ENABLE_Y_PIN, OUTPUT);
-  if (testing_x) {
-    digitalWrite(ENABLE_X_PIN, LOW);
-    digitalWrite(ENABLE_Y_PIN, HIGH);
-  } else {
-    digitalWrite(ENABLE_X_PIN, HIGH);
-    digitalWrite(ENABLE_Y_PIN, LOW);
+  digitalWrite(ENABLE_X_PIN, HIGH);
+  digitalWrite(ENABLE_Y_PIN, HIGH);
+}
+
+void setSelectSignal(int pin) {
+  for (int i = 0; i < NUM_SELECT; i++) {
+    if (pin & (1 << i)) {
+      digitalWrite(SELECT_PINS[NUM_SELECT - 1 - i], HIGH);
+    } else {
+      digitalWrite(SELECT_PINS[NUM_SELECT - 1 - i], LOW);
+    }
+  }
+}
+
+void cycleX() {
+  digitalWrite(ENABLE_X_PIN, LOW);
+  digitalWrite(ENABLE_Y_PIN, HIGH);
+  for (int i = 0; i < X_LEN; i++) {
+    setSelectSignal(i); 
+    touch_value = analogRead(OUTPUT_PIN); 
+    Serial << "x = " << i + 1<< ": " << touch_value << endl; 
+    delay(DELAY);
+  }
+}
+
+void cycleY() {
+  digitalWrite(ENABLE_X_PIN, HIGH);
+  digitalWrite(ENABLE_Y_PIN, LOW);
+  for (int i = 0; i < Y_LEN; i++) {
+    setSelectSignal(i); 
+    touch_value = analogRead(OUTPUT_PIN); 
+    Serial << "y = " << i + 1<< ": " << touch_value << endl; 
+    delay(DELAY);
   }
 }
 
 void loop()
 {
-
-  // digitalWrite(7, HIGH);   // S0-1
-  // digitalWrite(9, HIGH);   // S1-1
-  // digitalWrite(10, HIGH);  // S2-1
-  // digitalWrite(11, HIGH);  // S0-2
-  // digitalWrite(12, LOW);  // S1-2
-  // digitalWrite(13, LOW);  // S2-2
-
-  for (int j = 0; j < 48; j++) {
-
-    // sets select signals 
-    for (int i = 0; i < 6; i++) {
-      if (j & (1 << i)) {
-        digitalWrite(SELECT_PINS[5-i], HIGH);
-      } else {
-        digitalWrite(SELECT_PINS[5-i], LOW);
-      }
-    }
-
-    touch_value = analogRead(OUTPUT_PIN); 
-    Serial << "Pair" << j + 1<< ": " << touch_value << endl; 
-    delay(1000);
-  }
+  cycleX();
+  cycleY();
 }
